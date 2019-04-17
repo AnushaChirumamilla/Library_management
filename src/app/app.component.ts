95ff325b52a259d5c2fc7c1419c5bf310807062a
AnushaChirumamilla <48238270+AnushaChirumamilla@users.noreply.github.com>
2019-04-17 16:28:05 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Network } from '@ionic-native/network/ngx';
+import { Network } from '@ionic-native/network';
 import { NetworkServiceProvider } from './../NetworkServiceProvider';
 import { LoginPage } from './../pages/login/login';
 import { Component } from '@angular/core';
@@ -11,42 +11,43 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage: any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,public networkServiceProvider:NetworkServiceProvider,public network:Network) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public networkServiceProvider: NetworkServiceProvider, public network: Network) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
       this.checkNetworkConnectivity();
-  
+
     });
   }
-  checkNetworkConnectivity(){
-     // watch network for a disconnection
-        let disconnectSubscription = this.network.onDisconnect().subscribe(() => {
-            console.log('network was disconnected :-(');
-        });
+  checkNetworkConnectivity() {
+    // watch network for a disconnection
+    let disconnectSubscription = this.network.onDisconnect().subscribe(() => {
+      console.log('network was disconnected :-(');
+    });
 
-        // stop disconnect watch
-        disconnectSubscription.unsubscribe();
+    // stop disconnect watch
+    disconnectSubscription.unsubscribe();
 
 
-        // watch network for a connection
-        let connectSubscription = this.network.onConnect().subscribe(() => {
-            console.log('network connected!');
-            // We just got a connection but we need to wait briefly
-            // before we determine the connection type. Might need to wait.
-            // prior to doing any api requests as well.
-            setTimeout(() => {
-                if (this.network.type === 'wifi') {
-                    console.log('we got a wifi connection, woohoo!');
-                }
-            }, 3000);
-        });
+    // watch network for a connection
+    let connectSubscription = this.network.onConnect().subscribe(() => {
+      console.log('network connected!');
+      // We just got a connection but we need to wait briefly
+      // before we determine the connection type. Might need to wait.
+      // prior to doing any api requests as well.
+      setTimeout(() => {
+        if (this.network.type === 'wifi') {
+          console.log('we got a wifi connection, woohoo!');
+        }
+      }, 3000);
+    });
 
-        // stop connect watch
-        connectSubscription.unsubscribe();
+    // stop connect watch
+    connectSubscription.unsubscribe();
   }
 }
+

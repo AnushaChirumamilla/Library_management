5a14ea68a1c566b11d6f86663a1d09b49318ca96
AnushaChirumamilla <48238270+AnushaChirumamilla@users.noreply.github.com>
2019-04-17 16:27:21 +0530

    Update add-data.ts

--- src/pages/add-data/add-data.ts
+++ src/pages/add-data/add-data.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { Toast } from '@ionic-native/toast';
+import { Geolocation } from '@ionic-native/geolocation'
 
 @IonicPage()
 @Component({
@@ -9,40 +10,44 @@ import { Toast } from '@ionic-native/toast';
   templateUrl: 'add-data.html',
 })
 export class AddDataPage {
-
+  lat: number;
+  longt: number;
   data = { name: "", publisheddate: "", type: "", addeddate: "" };
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private sqlite: SQLite,
-    private toast: Toast) { }
+    private toast: Toast, private geolocation: Geolocation) { }
 
   saveData() {
+    this.getlocation()
     this.sqlite.create({
       name: 'ionicdb.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('INSERT INTO expense VALUES(NULL,?,?,?,?)', [this.data.name, this.data.publisheddate, this.data.type, this.data.addeddate])
+      db.executeSql("insert into books (name,publisheddate,type,addeddate) values('" + this.data.name + "','" + this.data.publisheddate + "','" + this.data.type + "','" + this.data.addeddate)
         .then(res => {
           console.log(res);
-        console.log('inserted')
+          console.log('inserted')
         })
         .catch(e => {
-          console.log(e);
-          this.toast.show(e, '5000', 'center').subscribe(
-            toast => {
-              console.log(toast);
-            }
-          );
+          console.log('notinserted' + e);
+
         });
     }).catch(e => {
-      console.log(e);
-      this.toast.show(e, '5000', 'center').subscribe(
-        toast => {
-          console.log(toast);
-        }
-      );
+      console.log('not success' + e);
+
+
     });
   }
+  getlocation() {
 
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = (resp.coords.latitude);
+      this.longt = (resp.coords.longitude);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
 }
+
